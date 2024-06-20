93d7d964cc736a3d33285752ee94dbab13619e01
Salman <salmanhaidar9576@gmail.com>
2024-06-20 11:08:32 -0400

    reviewed comparable, comparator

--- Section3_Advanced/Concepts/ConceptsCollection.java
+++ Section3_Advanced/Concepts/ConceptsCollection.java
@@ -1,12 +1,36 @@
 package Section3_Advanced.Concepts;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import Section3_Advanced.Concepts.a.b.c;
+
 import java.util.Iterator;
+
+class Student implements Comparable<Student>
+{
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if(this.age<o.age) 
+                    return 1;
+                else
+                    return -1;
+    }
+    
+}
 
 public class ConceptsCollection {
     public static void main(String[] args) {
@@ -100,6 +124,76 @@ public class ConceptsCollection {
         for (Integer key : map.keySet()) {
             System.out.println(key + " " + map.get(key));
         }
+
+        // Comparator vs Comparable
+        // Comparator is an interface in java.util package
+        // Comparator is used to sort objects of user-defined classes
+        // Comparator has compare() method
+        // Comparable is an interface in java.lang package
+        // Comparable is used to sort objects of user-defined classes
+        // Comparable has compareTo() method
+        // Comparable is implemented by the class whose objects are to be sorted
+        // Comparator is implemented by a separate class
+        // Difference is that Comparable is implemented in the class whose objects are to be sorted, while Comparator is implemented in a separate class
+
+        List<Integer> nums4 = new ArrayList<Integer>();
+        nums4.add(43);
+        nums4.add(31);
+        nums4.add(72);
+        nums4.add(29);
+
+        
+        Comparator<Integer> comp = new Comparator<Integer>()
+        {
+            
+            public int compare(Integer a, Integer b) {
+                if(a%10>b%10) 
+                    return 1;
+                else
+                    return -1;
+            }
+        };
+
+        Collections.sort(nums4,comp); 
+
+        System.out.println("Comparator" );
+        System.out.println(nums4);
+
+        // If you want a custom sorting order, you can use Comparator
+
+        List<Student> students = new ArrayList<Student>();
+
+        students.add(new Student(23, "Alice"));
+        students.add(new Student(27, "Bob"));
+        students.add(new Student(22, "Charlie"));
+        students.add(new Student(25, "David"));
+
+        for (Student student : students) {
+            System.out.println(student.age + " " + student.name);
+        }
+        
+        // ex with lambda: Comparator<Student> comp = (Student a, Student b) 
+        //-> a.age>b.age?1:-1;
+        Collections.sort(students, new Comparator<Student>() {
+            public int compare(Student a, Student b) {
+                if(a.age>b.age) 
+                    return 1;
+                else
+                    return -1;
+            }
+        });
+
+        System.out.println("Custom Comparator lower to higher");
+        for (Student student : students) {
+            System.out.println(student.age + " " + student.name);
+        }
+
+        System.out.println("Custom Comparator higher to lower");
+        Collections.sort(students);
+        for (Student student : students) {
+            System.out.println(student.age + " " + student.name);
+        }
+
 
     }
 }
